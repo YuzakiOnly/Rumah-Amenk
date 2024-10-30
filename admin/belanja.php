6b81dbe8a6ecd8701f8490c5c5ea3c77972bebcc
YuzakiOnly <aryamenk2007@gmail.com>
2024-10-30 13:29:50 +0700

    add:file

--- admin/belanja.php
+++ admin/belanja.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>Shopping Total and Discount Calculator</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pembelajaan</title>
 </head>
 <body>
     <div class="container">
-        <h1>Shopping Total and Discount Calculator</h1>
-        <form action="datamakanan.php" enctype="multipart/form-data" method="POST">
+        <h1>Pergi Untuk Belanja</h1>
+        <form action="belanja.php" enctype="multipart/form-data" method="POST">
             <label for="name">Nama Pembeli:</label>
             <input type="text" id="name" name="name" required>
 </br>
